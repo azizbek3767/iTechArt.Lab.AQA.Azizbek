41fd113f7e584455c9fd322909ee8d0f10a1be98
azizbekk3767 <azizbekk3767@gmail.com>
2023-01-06 16:10:47 +0500

    added a test file

---
+++ CalculatorTask/Tests/TestCalculator.cs
@@ -0,0 +1,104 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorTask.Tests
+{
+    [TestFixture]
+    [Parallelizable(ParallelScope.All)]
+    [FixtureLifeCycle(LifeCycle.SingleInstance)]
+    internal class TestCalculator
+    {
+        public Calculator Calculator { get; set; }
+
+        [OneTimeSetUp]
+        public void OneTimeSetup()
+        {
+            Console.Error.WriteLine("All tests are starting");
+        }
+
+        [SetUp]
+        public void Setup()
+        {
+            Calculator = new Calculator();
+        }
+
+        [Order(1)]
+        [Category("Calculator Test")]
+        [Test(Description = "This method tests the Add method of Calculator class", Author = "Azizbek")]
+        [TestCase(20, 10, 10)]
+        [TestCase(4.4, 2, 2.4)]
+        [TestCase(400.5454, 200.1, 200.4454)]
+        [TestCase(5, 5, 0)]
+        public void TestAdd(double expected, double firstNumber, double secondNumber)
+        {
+            Assert.AreEqual(expected, Math.Round(Calculator.Add(firstNumber, secondNumber)), 14);
+        }
+
+        [Order(2)]
+        [Category("Calculator Test")]
+        [Test(Description = "This method tests the Subtract method of Calculator class", Author = "Azizbek")]
+        [TestCase(10, 20, 10)]
+        [TestCase(2, 4.4, 2.4)]
+        [TestCase(200.1, 400.5454, 200.4454)]
+        [TestCase(5, 5, 0)]
+        public void TestSubtract(double expected, double firstNumber, double secondNumber)
+        {
+            Assert.AreEqual(expected, Math.Round(Calculator.Subtract(firstNumber, secondNumber)), 14);
+        }
+
+        [Order(3)]
+        [Category("Calculator Test")]
+        [Test(Description = "This method tests the Multiply method of Calculator class", Author = "Azizbek")]
+        [TestCase(100, 10, 10)]
+        [TestCase(4.8, 2, 2.4)]
+        [TestCase(40109.12454, 200.1, 200.4454)]
+        [TestCase(0, 5, 0)]
+        public void TestMultiply(double expected, double firstNumber, double secondNumber)
+        {
+            Assert.AreEqual(expected, Math.Round(Calculator.Multiply(firstNumber, secondNumber)), 14);
+        }
+
+        [Order(4)]
+        [Category("Calculator Test")]
+        [Test(Description = "This method tests the Divide method of Calculator class", Author = "Azizbek")]
+        [TestCaseSource(nameof(_fakeCredentials))]
+        public void TestDivision(double expected, double firstNumber, double secondNumber)
+        {
+            Assert.AreEqual(expected, Math.Round(Calculator.Divide(firstNumber, secondNumber)), 14);
+        }
+
+        private static object[] _fakeCredentials =
+        {
+            new object[] {1, 10, 10},
+            new object[] {0.83333333333333, 2, 2.4},
+            new object[] {0.99827683748292, 200.1, 200.4454},
+        };
+
+        [NonParallelizable]
+        [Order(5)]
+        [Category("Calculator Test")]
+        [Test(Description = "This method tests impossibility of division by 0 of Calculator class", Author = "Azizbek")]
+        //[Ignore("Bug")]
+        public void ImpossibilityOfDivisionByZero()
+        {
+            Assert.That(double.IsInfinity(Calculator.Divide(5, 0)), Is.EqualTo(true));
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Console.WriteLine("Test is finished");
+        }
+
+        [OneTimeTearDown]
+        public void OneTimeTearDown()
+        {
+            Console.Error.WriteLine("All tests completed");
+        }
+
+    }
+}
