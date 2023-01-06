3c4c4ef2c397ac84480f97345023311c37d20a1b
azizbekk3767 <azizbekk3767@gmail.com>
2023-01-06 15:57:54 +0500

    second task calculator test completed

---
+++ CalculatorTask/Calculator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorTask
+{
+    internal class Calculator
+    {
+        public double Add(double FirstNumber, double SecondNumber)
+        {
+            return FirstNumber + SecondNumber;
+        }
+        public double Subtract(double FirstNumber, double SecondNumber)
+        {
+            return FirstNumber - SecondNumber;
+        }
+        public double Multiply(double FirstNumber, double SecondNumber)
+        {
+            return FirstNumber * SecondNumber;
+        }
+        public double Divide(double FirstNumber, double SecondNumber)
+        {
+            return FirstNumber / SecondNumber;
+        }
+    }
+}
