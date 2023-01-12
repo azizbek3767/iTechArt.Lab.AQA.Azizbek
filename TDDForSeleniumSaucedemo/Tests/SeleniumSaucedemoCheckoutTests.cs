bf26265e689d817b2d0fac033fab1cfa3019b9a4
azizbekk3767 <azizbekk3767@gmail.com>
2023-01-12 22:55:44 +0500

    added TDD for Selenium tests

---
+++ TDDForSeleniumSaucedemo/Tests/SeleniumSaucedemoCheckoutTests.cs
@@ -0,0 +1,48 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+
+namespace TDDForSeleniumSaucedemo.Tests
+{
+    internal class SeleniumSaucedemoCheckoutTests
+    {
+        private IWebDriver _driver;
+
+        [SetUp]
+        public void SetupDriver()
+        {
+            _driver = new ChromeDriver();
+            _driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+            _driver.Manage().Window.Maximize();
+        }
+
+        [TearDown]
+        public void TeardownDriver()
+        {
+            _driver.Quit();
+        }
+
+        [Test]
+        public void Test()
+        {
+            var HomePage = new HomePage(_driver);
+
+            HomePage.Open("https://www.saucedemo.com/");
+            HomePage.FillInUsernameField("standard_user");
+            HomePage.FillInPasswordField("secret_sauce");
+            HomePage.ClickLoginButton();
+            Assert.AreEqual(HomePage.GetCurrentUrl(), "https://www.saucedemo.com/inventory.html");
+            Assert.IsTrue(HomePage.HomePageLogoExists());
+            HomePage.AddToCartButton.Click();
+            HomePage.CartButton.Click();
+            Assert.IsTrue(HomePage.IsCartInventoryExists());
+            Assert.IsTrue(HomePage.IsRightItemIsAddedToCart());
+            HomePage.Checkout();
+            HomePage.FillInCheckoutForm();
+            HomePage.CheckoutContinueButton.Click();
+            Assert.AreEqual(HomePage.GetCurrentUrl(), "https://www.saucedemo.com/checkout-step-two.html");
+            HomePage.CheckoutFinishButton.Click();
+            Assert.AreEqual(HomePage.GetCurrentUrl(), "https://www.saucedemo.com/checkout-complete.html");
+        }
+    }
+}
---
+++ TDDForSeleniumSaucedemo/Tests/SeleniumSaucedemoLoginTests.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+
+namespace TDDForSeleniumSaucedemo.Tests
+{
+    internal class SeleniumSaucedemoLoginTests
+    {
+        private IWebDriver _driver;
+
+        [SetUp]
+        public void SetupDriver()
+        {
+            _driver = new ChromeDriver();
+            _driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+            _driver.Manage().Window.Maximize();
+        }
+
+        [TearDown]
+        public void TeardownDriver()
+        {
+            _driver.Quit();
+        }
+
+        [Test]
+        public void Test()
+        {
+            var HomePage = new HomePage(_driver);
+
+            HomePage.Open("https://www.saucedemo.com/");
+            HomePage.FillInUsernameField("standard_user");
+            HomePage.FillInPasswordField("secret_sauce");
+            HomePage.ClickLoginButton();
+            Assert.AreEqual(HomePage.GetCurrentUrl(), "https://www.saucedemo.com/inventory.html");
+            Assert.IsTrue(HomePage.HomePageLogoExists());
+        }
+    }
+}
