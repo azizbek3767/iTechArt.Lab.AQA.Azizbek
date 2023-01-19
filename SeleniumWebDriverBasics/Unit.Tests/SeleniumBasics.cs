cc8ca77aaa4f71ce456b939ddbbde427bb37484b
azizbekk3767 <azizbekk3767@gmail.com>
2023-01-20 02:51:38 +0500

    improved the project and fixed bugs

--- SeleniumWebDriverBasics/Unit.Tests/SeleniumBasics.cs
+++ SeleniumWebDriverBasics/Unit.Tests/SeleniumBasics.cs
@@ -2,6 +2,10 @@
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Interactions;
+using SeleniumWebDriverBasics.Locators;
+using Microsoft.Extensions.Configuration;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
 
 namespace SeleniumWebDriverBasics.Unit.Tests
 {
@@ -9,13 +13,17 @@ namespace SeleniumWebDriverBasics.Unit.Tests
     internal class SeleniumBasics
     {
         private IWebDriver _driver;
+        private IJavaScriptExecutor _executor;
+
+        private IConfigurationBuilder _builder;
 
         [SetUp]
         public void SetupDriver()
         {
             _driver = new ChromeDriver();
-            _driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            _driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(7);
             _driver.Manage().Window.Maximize();
+            _executor = (IJavaScriptExecutor)_driver;
         }
 
         [Test]
@@ -35,40 +43,46 @@ namespace SeleniumWebDriverBasics.Unit.Tests
 
             // 3.Из списка производителей выбрать: Apple, Honor
             Actions actions = new Actions(_driver);
-            actions.SendKeys(Keys.PageDown).Build().Perform();
-            _driver.FindElement(By.XPath("//*[@id='schema-filter']/div[5]/div[4]/div[2]/ul/li[1]/label/span[2]")).Click();
-            Assert.That(_driver.FindElement(By.XPath("//*[@id='schema-filter']/div[5]/div[4]/div[2]/ul/li[1]/label/span[1]/input")).Selected, Is.True);
-            _driver.FindElement(By.XPath("//*[@id='schema-filter']/div[5]/div[4]/div[2]/ul/li[5]/label/span[2]")).Click();
-            Assert.That(_driver.FindElement(By.XPath("//*[@id='schema-filter']/div[5]/div[4]/div[2]/ul/li[5]/label/span[1]/input")).Selected, Is.True);
+            var AppleManufacturerSpan = _driver.FindElement(HomePageLocators.AppleManufacturerSpanLocator);
+            _executor.ExecuteScript("arguments[0].click();", AppleManufacturerSpan);
+            Assert.That(_driver.FindElement(HomePageLocators.AppleManufacturerInputLocator).Selected, Is.True);
+
+            var HonorManufacturerSpan = _driver.FindElement(HomePageLocators.HonorManufacturerSpanLocator);
+            _executor.ExecuteScript("arguments[0].click();", HonorManufacturerSpan);
+            Assert.That(_driver.FindElement(HomePageLocators.HonorManufacturerInputLocator).Selected, Is.True);
 
             // 4.В отобразившемся списке удалить Honor
-            _driver.FindElement(By.XPath("//*[@id='schema-filter']/div[5]/div[4]/div[2]/ul/li[5]/label/span[2]")).Click();
-            Assert.That(_driver.FindElement(By.XPath("//*[@id='schema-filter']/div[5]/div[4]/div[2]/ul/li[5]/label/span[1]/input")).Selected, Is.False);
+            _executor.ExecuteScript("arguments[0].click();", HonorManufacturerSpan);
+            Assert.That(_driver.FindElement(HomePageLocators.HonorManufacturerInputLocator).Selected, Is.False);
+
             // Ожидаемый результат: В списке нет моделей с назвнием Honor
-            var Phones = _driver.FindElements(By.XPath("//div[@class='schema-product__group']/div/div[3]/div[2]/div/a/span"));
+            var Phones = _driver.FindElements(HomePageLocators.PhonesListLocator);
             foreach(var Phone in Phones)
             {
                 Assert.That(Phone.Text, Does.Not.Contain("Honor"));
             }
 
             // 5.Из списка оставшихся телефоном выбрать 1-ый и 3-ий (поставить чекбокс)
-            actions.SendKeys(Keys.PageUp).Build().Perform();
-            var FirstItemCheckbox = _driver.FindElement(By.XPath("//div[@class='schema-product__group']/div/div/div/div/label/span/input[1]"));
-            FirstItemCheckbox.Click();
-            Assert.That(FirstItemCheckbox.Selected, Is.True);
+            var wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(10));
+            wait.Until(ExpectedConditions.ElementIsVisible(By.XPath("//*[@id=\"schema-products\"]/div[2]/div/div[3]/div[2]/div[1]/a/span[contains(text(), 'Apple')]")));
+           
+            var FirstItemCheckboxLabel = _driver.FindElement(HomePageLocators.FirstItemCheckboxLabel);
+            _executor.ExecuteScript("arguments[0].click();", FirstItemCheckboxLabel);
+            var FirstItemCheckboxInput = _driver.FindElement(HomePageLocators.FirstItemCheckboxInput);
+            Assert.IsTrue(FirstItemCheckboxInput.Selected);
 
-            actions.SendKeys(Keys.PageDown).Build().Perform();
-            actions.SendKeys(Keys.PageDown).Build().Perform();
-            var ThirdItemCheckbox = _driver.FindElement(By.XPath("//div[@class='schema-product__group']/div/div/div/div/label/span/input[3]"));
-            ThirdItemCheckbox.Click();
-            Assert.That(ThirdItemCheckbox.Selected, Is.True);
+            var ThirdItemCheckboxLabel = _driver.FindElement(HomePageLocators.ThirdItemCheckboxLabel);
+            _executor.ExecuteScript("arguments[0].click();", ThirdItemCheckboxLabel);
+            var ThirdItemCheckboxInput = _driver.FindElement(HomePageLocators.ThirdItemCheckboxInput);
+            Assert.IsTrue(ThirdItemCheckboxInput.Selected);
 
             //Ожидаемый результат: Проверить, что в сравнении 2 товара
             Assert.That(HomePage.SpanForComparison.Text, Is.EqualTo("2 товара"));
 
             //6.Открыть страницу Сравнения
-            //Ожидаемый результат: Страница сравнения открыта
             actions.MoveToElement(HomePage.ButtonWithTwoElementsToCompare).Click().Build().Perform();
+            //Ожидаемый результат: Страница сравнения открыта
+            Assert.AreEqual("https://catalog.onliner.by/compare/appl2bmwlt2+appl2amq6g2", _driver.Url);
 
             //7.Навести курсор на «Описание» 
             //Ожидаемый результат: Окно с текстом открылось
@@ -78,32 +92,9 @@ namespace SeleniumWebDriverBasics.Unit.Tests
             Assert.That(HomePage.SpanWithOverview.Text, Is.EqualTo("Описание"));
 
             //9.Открыть предыдущую страницу
-            //Ожидаемый результат: Страница со списков телефонов Apple открыта
             _driver.Navigate().Back();
-
-
-
-
-            // The below code is the old version 
-
-            /*Actions actions = new Actions(_driver);
-            actions.SendKeys(Keys.PageDown).Build().Perform();
-            //actions.MoveToElement(HomePage.AppleManufacturerCheckbox);
-            HomePage.AppleManufacturerCheckbox.Click();
-            Assert.That(HomePage.AppleCheckboxInput.Selected, Is.True);
-            //actions.MoveToElement(HomePage.HonorManufacturerCheckbox).Click().Build().Perform();
-            HomePage.HonorManufacturerCheckbox.Click();
-            Assert.That(HomePage.HonorCheckboxInput.Selected, Is.True);
-            actions.MoveToElement(HomePage.HonorManufacturerCheckbox).Click().Build().Perform();
-            actions.MoveToElement(HomePage.FirstItemWithClass).Click().Build().Perform();
-            //HomePage.FirstItemWithClass.Click();
-            actions.MoveToElement(HomePage.ThirdItemWithClass).Click().Build().Perform();
-            //HomePage.ThirdItemWithClass.Click();
-            Assert.That(HomePage.SpanForComparison.Text, Is.EqualTo("2 товара"));
-            actions.MoveToElement(HomePage.ButtonWithTwoElementsToCompare).Click().Build().Perform();
-            actions.MoveToElement(HomePage.SpanWithOverview).Perform();
-            Assert.That(HomePage.SpanWithOverview.Text, Is.EqualTo("Описание"));
-            _driver.Navigate().Back();*/
+            //Ожидаемый результат: Страница со списков телефонов Apple открыта
+            Assert.AreEqual("https://catalog.onliner.by/mobile/apple", _driver.Url);
         }
 
         [TearDown]
