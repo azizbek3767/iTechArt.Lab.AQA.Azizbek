182d51a3c8aa207896ed95add0f6a15a1630e089
azizbekk3767 <azizbekk3767@gmail.com>
2023-02-10 11:21:35 +0500

    added the functionality for retrieving data from appsettings.json

--- AllureReport/Configurations/AppConfiguration.cs
+++ AllureReport/Configurations/AppConfiguration.cs
@@ -9,12 +9,11 @@ namespace SeleniumAdvancedPartTwo.Configurations
 
         private const string UrlKey = "url";
 
-        public static readonly Browser Browser = Browser.Chrome;
-        //Enum.Parse<Browser>(Configurator.GetConfigurator().GetSection(BrowserKey).Value, true);
-
-        public static readonly string Url = "http://admin:admin@the-internet.herokuapp.com";
-        //Configurator.GetConfigurator().GetSection(UrlKey).Value;
-        public static readonly int ConditionTimeout = 10;
-            //Convert.ToInt32(Configurator.GetConfigurator().GetSection("conditionTimeout").Value);
+        public static readonly Browser Browser = 
+            Enum.Parse<Browser>(Configurator.GetConfigurator().GetSection(BrowserKey).Value, true);
+        public static readonly string Url = 
+            Configurator.GetConfigurator().GetSection(UrlKey).Value;
+        public static readonly int ConditionTimeout = 
+            Convert.ToInt32(Configurator.GetConfigurator().GetSection("conditionTimeout").Value);
     }
 }
