729b481abfa284850627217f40467b3b8d0b9a09
azizbekk3767 <azizbekk3767@gmail.com>
2023-02-23 23:59:09 +0500

    renames the BaseElement class to BaseForm

---
+++ SeleniumWrapper.Core/Elements/BaseForm.cs
@@ -0,0 +1,51 @@
+﻿using OpenQA.Selenium;
+using SeleniumWrapper.Core.Actions;
+using SeleniumWrapper.Core.BrowserUtils;
+using SeleniumWrapper.Core.Utilities;
+
+namespace SeleniumWrapper.Core.Elements
+{
+    public abstract class BaseForm
+    {
+        protected By Locator { get; }
+
+        protected string Name { get; }
+
+        protected BaseForm(By locator, string name)
+        {
+            Locator = locator;
+            Name = name;
+        }
+        protected MouseActions MouseActions => new MouseActions(WebDriver);
+        private WebDriver WebDriver
+        {
+            get { return BrowserService.Browser.WebDriver; }
+        }
+
+        public void Click()
+        {
+            FindElement().Click();
+        }
+        public void SimpleClick()
+        {
+            WebDriver.FindElement(Locator).Click();
+        }
+
+        public string GetText()
+        {
+            return FindElement().Text;
+        }
+
+        public bool IsDisplayed()
+        {
+            return FindElement().Displayed;
+        }
+
+        protected IWebElement FindElement()
+        {
+            return ElementFinder.GetWebElement(Locator);
+        }
+
+        private ElementFinder ElementFinder => new ElementFinder(WebDriver);
+    }
+}
