f3e7f265c7b0ff4aaa6965f84a223712eb9e56ca
azizbekk3767 <azizbekk3767@gmail.com>
2023-03-07 17:17:57 +0500

    all

---
+++ ApiTests/ApiTests.cs
@@ -0,0 +1,40 @@
+﻿using RestSharp;
+using System.Net;
+
+namespace ApiTests
+{
+    public class ApiTests
+    {
+        [Fact]
+        public void Onliner_Restsharp_Tests_Demo()
+        {
+            //Arrange
+            var client = new RestClient("https://onliner.by");
+            var restRequest = new RestRequest();
+            
+            //Act
+            var response = client.Execute(restRequest);
+
+            //Assert
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            Assert.NotEqual(string.Empty, response.Content);
+        }
+
+        [Fact]
+        public void PetSwagger_Restsharp_Tests_Demo()
+        {
+            //Arrange
+            var client = new RestClient("https://petstore.swagger.io");
+            var restRequest = new RestRequest("/v2/user", Method.Post);
+            var body = "{\r\n  \"id\": 999,\r\n  \"username\": \"azizbek3767\",\r\n  \"firstName\": \"Azizbek\",\r\n  \"lastName\": \"Nasriddinov\",\r\n  \"email\": \"string\",\r\n  \"password\": \"pass\",\r\n  \"phone\": \"123456789\",\r\n  \"userStatus\": 1\r\n}";
+            
+            //Act
+            restRequest.AddJsonBody(body);
+            var response = client.Execute(restRequest);
+
+            //Assert
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            Assert.NotEqual(string.Empty, response.Content);
+        }
+    }
+}
---
+++ ApiTests/JsonPlaceholderTests.cs
@@ -0,0 +1,37 @@
+﻿using RestSharp;
+using System.Collections.Immutable;
+using System.Net;
+using Xunit.Abstractions;
+
+namespace ApiTests
+{
+    public class JsonPlaceholderTests
+    {
+        private ITestOutputHelper _output;
+
+        public JsonPlaceholderTests(ITestOutputHelper output)
+        {
+            _output = output;
+        }
+
+        [Fact]
+        public void FirstTest()
+        {
+            //Arrange
+            var client = new RestClient("https://jsonplaceholder.typicode.com");
+            var restRequest = new RestRequest("/posts", Method.Get);
+
+            //Act
+            var response = client.Execute(restRequest);
+
+            foreach(var item in response.Content)
+            {
+                _output.WriteLine(item.ToString());
+            }
+
+            //Assert
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            Assert.NotEqual(string.Empty, response.Content);
+        }
+    }
+}
